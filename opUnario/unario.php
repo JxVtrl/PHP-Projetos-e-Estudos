6de68a11f12c5160a89d8af220a9b3131529072a
João Vinicius Vitral <joaoviniciusvitral@hotmail.com>
2021-06-21 15:50:13 -0300

    :tada: Operador Unário

---
+++ opUnario/unario.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Operador Unário</title>
+    <link rel="stylesheet" href="../_css/estilo.css">
+</head>
+<body>
+<div>
+    <?php
+        echo "<h3>Operador Unário</h3>";
+        echo "(expressão)?verdadeiro:falso";
+        $x = 5;
+        $y = 6;
+        $r = $x>$y ? $x+$y : $x*$y;
+        echo "</br>x = $x</br>y = $y";
+        echo "</br>$x>$y ? $x+$y : $x*$y";       
+        echo "</br>Resposta: $r ";
+        echo "<h3>Exemplo Prático</h3>";
+        echo "";
+
+        ?>
+</div>    
+</body>
+</html>
